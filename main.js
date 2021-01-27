1340c65247a2d9f8dc71b37d8a14ded839c23d48
Malo <ndekezi37@gmail.com>
2021-01-27 14:41:35 +0100

    correction

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-let content = document.querySelector('#content');
-console.log(content);
+//1
+let idContent = document.querySelector('#content')
+console.log(idContent);
 
-let contentAll = document.querySelectorAll('#content');
-console.log(contentAll);
+//2
+let idContentAll = document.querySelectorAll('#content')
+console.log(idContentAll);
 
-let liste = document.querySelector('#listElements');
-let listeAll = liste.querySelectorAll('li');
-let listeFinal = Array.from(listeAll) 
-console.log(listeFinal);
-listeFinal.forEach(el =>{
-    console.log(el);
+//3
+let classImportant = document.querySelector('.important')
+console.log(classImportant);
+
+//4
+let classImportantAll = document.querySelectorAll('.important')
+console.log(classImportantAll);
+
+//5
+let liAll = document.querySelectorAll('li')
+let monTabLi = Array.from(liAll)
+
+monTabLi.forEach(el => {
+    let monText = el.innerText
+    console.log(monText.substr(0, monText.length-1) + monText.substr(-1).toUpperCase());
 });
 
 
+//6
+const d = document.getElementById("content"); 
+const p = d.getElementsByClassName('grandParagraphe');
+console.log(p); 
 
+//6 version query
+let idContent2 = document.querySelector('#content')
+let paraInId = idContent2.querySelector('.grandParagraphe')
+console.log(paraInId);
